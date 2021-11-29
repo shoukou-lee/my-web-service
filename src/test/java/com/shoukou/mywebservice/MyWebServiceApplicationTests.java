fb86e85bbbe92b7e0788facc5ecaa976e5e954bf
shoukou-lee <lee_seungho@inha.edu>
2021-11-29 23:04:32 +0900

    Initial commit

---
+++ src/test/java/com/shoukou/mywebservice/MyWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoukou.mywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
