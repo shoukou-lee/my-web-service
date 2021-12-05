0d6fee077c0710a53feea4214e245ee12a5f964a
shoukou-lee <lee_seungho@inha.edu>
2021-12-06 00:29:57 +0900

    ~p.194

--- src/main/java/com/shoukou/mywebservice/web/IndexController.java
+++ src/main/java/com/shoukou/mywebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.shoukou.mywebservice.web;
 
+import com.shoukou.mywebservice.config.auth.dto.SessionUser;
 import com.shoukou.mywebservice.service.posts.PostsService;
 import com.shoukou.mywebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
@@ -33,5 +42,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
 }
